318d36f2edd838d7d448032a5f5a657de31dc02d
sam-sloan <samforderer@gmail.com>
2016-08-25 01:59:22 -0400

    added buttons, extra styles, updated layout. I also added social links to the footer

--- js/script.js
+++ js/script.js
@@ -17,5 +17,21 @@ $(document).ready(function(){
     });
   });
 
+  scrolling()
+
 
 });
+
+function scrolling() {
+  //When the arrow has been clicked...
+  $(".intro a").click(function(e){
+    //prevent default will prevent the link from typical behaviour of page jumping
+    e.preventDefault();
+
+    var sectionID = "headerSection";
+
+    $("html body").animate({
+      scrollTop : $("#" + sectionID).offset().top
+    }, 700)
+  })
+}
