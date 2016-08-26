f40a4640523ec7cc61cc56a506e19461335926ff
sam-sloan <samforderer@gmail.com>
2016-08-26 11:03:55 -0400

    final touches to layout. added new background imageto resources for the apparel section. added color to social media icons hover effect

--- js/script.js
+++ js/script.js
@@ -1,29 +1,6 @@
 $(document).ready(function(){
 
-  $(window).scroll(function(){
-
-    var wSkullScroll = $(this).scrollTop();
-    var wScroll = $(this).scrollTop();
-
-    $('.skully').css({
-        'transform': 'translate(0px, '+ wSkullScroll /15 + '%)',
-        'filter': 'blur('+ wScroll / 29 + 'px)',
-        '-webkit-filter': 'blur('+ wScroll / 29 + 'px)'
-
-    });
-
-    $('.triangle').css({
-      'transform': 'translate(0px, '+ wScroll /3 + '%)'
-    });
-  });
-
-  scrolling()
-
-
-});
-
-function scrolling() {
-  //When the arrow has been clicked...
+    //When the arrow has been clicked...
   $(".intro a").click(function(e){
     //prevent default will prevent the link from typical behaviour of page jumping
     e.preventDefault();
@@ -33,5 +10,23 @@ function scrolling() {
     $("html body").animate({
       scrollTop : $("#" + sectionID).offset().top
     }, 700)
-  })
-}
+  });
+
+
+  $(window).scroll(function(){
+
+    var wSkullScroll = $(this).scrollTop();
+    var wScroll = $(this).scrollTop();
+
+    $('.skully').css({
+        'transform': 'translate(0px, '+ wSkullScroll /5 + '%)',
+        'filter': 'blur('+ wScroll / 29 + 'px)',
+        '-webkit-filter': 'blur('+ wScroll / 29 + 'px)'
+
+    });
+
+    $('.triangle').css({
+      'transform': 'translate(0px, '+ wScroll /3 + '%)'
+    });
+  });
+});
